98f3311c07412e5e67fe9f1a501749542c928403
hunit <hunit612@naver.com>
2023-08-05 23:57:23 +0900

    feat: add mongoos

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const PORT = 3000;
 
 const app = express();
 const productRoutes = require('./routes')
+const username = encodeURIComponent("hunit612");
+const password = encodeURIComponent("dlrlgns78?");
+const mongoose = require('mongoose')
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.9nrsdmt.mongodb.net/`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => console.log('MongoDb Connected...'))
+    .catch(err => console.log(err));
 
 app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
